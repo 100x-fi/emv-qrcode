78111cfface318a8347f57dd1327144e8d317f65
Tharit Nildam <bom@r0ix.com>
2020-09-25 13:04:13 +0700

    feat: decode cpqr with custom field BERTLV

--- emv/cpm/emv_cpm.go
+++ emv/cpm/emv_cpm.go
@@ -4,36 +4,10 @@ import (
 	"encoding/base64"
 	"encoding/hex"
 	"fmt"
+	"log"
 	"strconv"
+	"strings"
 	"unicode/utf8"
-)
-
-// const ...
-const (
-	IDPayloadFormatIndicator                 = "85" // (M) Payload Format Indicator
-	IDApplicationTemplate                    = "61" // (M) Application Template
-	IDCommonDataTemplate                     = "62" // (O) Common Data Template
-	IDApplicationSpecificTransparentTemplate = "63" // (O) Application Specific Transparent Template
-	IDCommonDataTransparentTemplate          = "64" // (O) Common Data Transparent Template
-)
-
-// const ...
-const (
-	TagApplicationDefinitionFileName = "4F"
-	TagApplicationLabel              = "50"
-	TagTrack2EquivalentData          = "57"
-	TagApplicationPAN                = "5A"
-	TagCardholderName                = "5F20"
-	TagLanguagePreference            = "5F2D"
-	TagIssuerURL                     = "5F50"
-	TagApplicationVersionNumber      = "9F08"
-	TagIssuerApplicationData         = "9F10"
-	TagTokenRequestorID              = "9F19"
-	TagPaymentAccountReference       = "9F24"
-	TagLast4DigitsOfPAN              = "9F25"
-	TagApplicationCryptogram         = "9F26"
-	TagApplicationTransactionCounter = "9F36"
-	TagUnpredictableNumber           = "9F37"
 )
 
 // EMVQR ...
@@ -41,6 +15,14 @@ type EMVQR struct {
 	DataPayloadFormatIndicator string                // 85
 	ApplicationTemplates       []ApplicationTemplate // 61
 	CommonDataTemplates        []CommonDataTemplate  // 62
+	customBERTLVMapID4         map[string]*CustomBERTLVField
+	customBERTLVMapID2         map[string]*CustomBERTLVField
+}
+
+// CustomBERTLVField ...
+type CustomBERTLVField struct {
+	Description string
+	IsHex       bool
 }
 
 // ApplicationTemplate ...
@@ -82,6 +64,37 @@ type BERTLV struct {
 	DataApplicationCryptogram         string // "9F26"
 	DataApplicationTransactionCounter string // "9F36"
 	DataUnpredictableNumber           string // "9F37"
+	AdditionalDataMap                 map[string]string
+}
+
+// AddAdditionalData ...
+func (b *BERTLV) AddAdditionalData(id, val string) {
+	if b.AdditionalDataMap == nil {
+		b.AdditionalDataMap = make(map[string]string)
+	}
+	b.AdditionalDataMap[id] = val
+}
+
+// AddCustomBERTLVID4 ...
+func (c *EMVQR) AddCustomBERTLVID4(id, description string, isHex bool) {
+	if c.customBERTLVMapID4 == nil {
+		c.customBERTLVMapID4 = make(map[string]*CustomBERTLVField)
+	}
+	c.customBERTLVMapID4[id] = &CustomBERTLVField{
+		Description: description,
+		IsHex:       isHex,
+	}
+}
+
+// AddCustomBERTLVID2 ...
+func (c *EMVQR) AddCustomBERTLVID2(id, description string, isHex bool) {
+	if c.customBERTLVMapID2 == nil {
+		c.customBERTLVMapID2 = make(map[string]*CustomBERTLVField)
+	}
+	c.customBERTLVMapID2[id] = &CustomBERTLVField{
+		Description: description,
+		IsHex:       isHex,
+	}
 }
 
 // GeneratePayload ...
@@ -124,6 +137,309 @@ func (c *EMVQR) GeneratePayload() (string, error) {
 	return s, nil
 }
 
+func (c *EMVQR) Decode(payload string) (*EMVQR, error) {
+	s, err := base64.StdEncoding.DecodeString(payload)
+	if err != nil {
+		return nil, err
+	}
+	log.Println(string(s))
+	encoded := hex.EncodeToString(s)
+	log.Println(encoded)
+	emvqr := new(EMVQR)
+	p := NewParser(encoded)
+	idWordCount := IDWordCount
+	for p.Next(idWordCount) {
+		id := strings.ToUpper(string(p.ID(idWordCount)))
+		length := p.ValueLength(idWordCount)
+		hexValue := p.Value(idWordCount)
+		log.Printf("[Payload] id: %v, length: %v, hexValue: %v", id, length, hexValue)
+		switch id {
+		case IDPayloadFormatIndicator:
+			value, err := fromHex(hexValue)
+			if err != nil {
+				return nil, err
+			}
+			emvqr.DataPayloadFormatIndicator = value
+		case IDApplicationTemplate:
+			applicationTemplate, err := c.ParseApplication(hexValue)
+			if err != nil {
+				return nil, err
+			}
+			emvqr.ApplicationTemplates = append(emvqr.ApplicationTemplates, *applicationTemplate)
+		case IDCommonDataTemplate:
+			commonDataTemplate, err := c.ParseCommonDataTemplate(hexValue)
+			if err != nil {
+				return nil, err
+			}
+			emvqr.CommonDataTemplates = append(emvqr.CommonDataTemplates, *commonDataTemplate)
+		default:
+			// nothing
+		}
+	}
+	return emvqr, nil
+}
+
+// ParseApplication ...
+func (c *EMVQR) ParseApplication(hexString string) (*ApplicationTemplate, error) {
+	applicationTemplate := new(ApplicationTemplate)
+	p := NewParser(hexString)
+	idWordCount := IDWordCount
+	for p.Next(idWordCount) {
+		idWordCount = IDWordCount
+		id := strings.ToUpper(string(p.ID(idWordCount)))
+		length := p.ValueLength(idWordCount)
+		hexVal := p.Value(idWordCount)
+		value, err := fromHex(hexVal)
+		if err != nil {
+			return nil, err
+		}
+		log.Printf("[ID length 2] id: %v, length: %v, hexValue: %v, value: %v", id, length, hexVal, value)
+		switch id {
+		case TagApplicationDefinitionFileName:
+			applicationTemplate.DataApplicationDefinitionFileName = value
+		case TagApplicationLabel:
+			applicationTemplate.DataApplicationLabel = value
+		case TagTrack2EquivalentData:
+			applicationTemplate.DataTrack2EquivalentData = hexVal
+		case TagApplicationPAN:
+			applicationTemplate.DataApplicationPAN = hexVal
+		case IDApplicationSpecificTransparentTemplate:
+			bertlv, err := c.ParseBERTLV(hexVal)
+			if err != nil {
+				return nil, err
+			}
+			applicationTemplate.ApplicationSpecificTransparentTemplates = append(
+				applicationTemplate.ApplicationSpecificTransparentTemplates,
+				ApplicationSpecificTransparentTemplate{
+					*bertlv,
+				},
+			)
+		default:
+			if c.customBERTLVMapID2[id] != nil {
+				if c.customBERTLVMapID2[id].IsHex {
+					applicationTemplate.AddAdditionalData(id, hexVal)
+				} else {
+					applicationTemplate.AddAdditionalData(id, value)
+				}
+			} else {
+				idWordCount = 4
+				id = strings.ToUpper(string(p.ID(idWordCount)))
+				length = p.ValueLength(idWordCount)
+				hexVal = p.Value(idWordCount)
+				value, err = fromHex(hexVal)
+				if err != nil {
+					return nil, err
+				}
+				log.Printf("[ID length 4] id: %v, length: %v, hexValue: %v, value: %v", id, length, hexVal, value)
+				switch id {
+				case TagCardholderName:
+					applicationTemplate.DataCardholderName = value
+				case TagLanguagePreference:
+					applicationTemplate.DataLanguagePreference = value
+				case TagIssuerURL:
+					applicationTemplate.DataIssuerURL = value
+				case TagApplicationVersionNumber:
+					applicationTemplate.DataApplicationVersionNumber = hexVal
+				case TagIssuerApplicationData:
+					applicationTemplate.DataIssuerApplicationData = hexVal
+				case TagTokenRequestorID:
+					applicationTemplate.DataTokenRequestorID = hexVal
+				case TagPaymentAccountReference:
+					applicationTemplate.DataPaymentAccountReference = hexVal
+				case TagLast4DigitsOfPAN:
+					applicationTemplate.DataLast4DigitsOfPAN = hexVal
+				case TagApplicationCryptogram:
+					applicationTemplate.DataApplicationCryptogram = hexVal
+				case TagApplicationTransactionCounter:
+					applicationTemplate.DataApplicationTransactionCounter = hexVal
+				case TagUnpredictableNumber:
+					applicationTemplate.DataUnpredictableNumber = hexVal
+				default:
+					if c.customBERTLVMapID4[id] != nil {
+						if c.customBERTLVMapID4[id].IsHex {
+							applicationTemplate.AddAdditionalData(id, hexVal)
+						} else {
+							applicationTemplate.AddAdditionalData(id, value)
+						}
+					}
+					// skip unknown
+				}
+			}
+		}
+	}
+	return applicationTemplate, nil
+}
+
+func (c *EMVQR) ParseCommonDataTemplate(hexString string) (*CommonDataTemplate, error) {
+	commonDataTemplate := new(CommonDataTemplate)
+	p := NewParser(hexString)
+	idWordCount := IDWordCount
+	for p.Next(idWordCount) {
+		idWordCount = IDWordCount
+		id := strings.ToUpper(string(p.ID(idWordCount)))
+		length := p.ValueLength(idWordCount)
+		hexVal := p.Value(idWordCount)
+		value, err := fromHex(hexVal)
+		if err != nil {
+			return nil, err
+		}
+		log.Printf("[ID length 2] id: %v, length: %v, hexValue: %v, value: %v", id, length, hexVal, value)
+		switch id {
+		case TagApplicationDefinitionFileName:
+			commonDataTemplate.DataApplicationDefinitionFileName = value
+		case TagApplicationLabel:
+			commonDataTemplate.DataApplicationLabel = value
+		case TagTrack2EquivalentData:
+			commonDataTemplate.DataTrack2EquivalentData = hexVal
+		case TagApplicationPAN:
+			commonDataTemplate.DataApplicationPAN = hexVal
+		case IDCommonDataTransparentTemplate:
+			bertlv, err := c.ParseBERTLV(hexVal)
+			if err != nil {
+				return nil, err
+			}
+			commonDataTemplate.CommonDataTransparentTemplates = append(
+				commonDataTemplate.CommonDataTransparentTemplates,
+				CommonDataTransparentTemplate{
+					*bertlv,
+				},
+			)
+		default:
+			if c.customBERTLVMapID2[id] != nil {
+				if c.customBERTLVMapID2[id].IsHex {
+					commonDataTemplate.AddAdditionalData(id, hexVal)
+				} else {
+					commonDataTemplate.AddAdditionalData(id, value)
+				}
+			} else {
+				idWordCount = 4
+				id = strings.ToUpper(string(p.ID(idWordCount)))
+				length = p.ValueLength(idWordCount)
+				hexVal = p.Value(idWordCount)
+				value, err = fromHex(hexVal)
+				if err != nil {
+					return nil, err
+				}
+				log.Printf("[ID length 4] id: %v, length: %v, hexValue: %v, value: %v", id, length, hexVal, value)
+				switch id {
+				case TagCardholderName:
+					commonDataTemplate.DataCardholderName = value
+				case TagLanguagePreference:
+					commonDataTemplate.DataLanguagePreference = value
+				case TagIssuerURL:
+					commonDataTemplate.DataIssuerURL = value
+				case TagApplicationVersionNumber:
+					commonDataTemplate.DataApplicationVersionNumber = hexVal
+				case TagIssuerApplicationData:
+					commonDataTemplate.DataIssuerApplicationData = hexVal
+				case TagTokenRequestorID:
+					commonDataTemplate.DataTokenRequestorID = hexVal
+				case TagPaymentAccountReference:
+					commonDataTemplate.DataPaymentAccountReference = hexVal
+				case TagLast4DigitsOfPAN:
+					commonDataTemplate.DataLast4DigitsOfPAN = hexVal
+				case TagApplicationCryptogram:
+					commonDataTemplate.DataApplicationCryptogram = hexVal
+				case TagApplicationTransactionCounter:
+					commonDataTemplate.DataApplicationTransactionCounter = hexVal
+				case TagUnpredictableNumber:
+					commonDataTemplate.DataUnpredictableNumber = hexVal
+				default:
+					if c.customBERTLVMapID4[id] != nil {
+						if c.customBERTLVMapID4[id].IsHex {
+							commonDataTemplate.AddAdditionalData(id, hexVal)
+						} else {
+							commonDataTemplate.AddAdditionalData(id, value)
+						}
+					}
+					// skip unknown
+				}
+			}
+		}
+	}
+	return commonDataTemplate, nil
+}
+
+// ParseBERTLV ...
+func (c *EMVQR) ParseBERTLV(hexString string) (*BERTLV, error) {
+	bertlv := new(BERTLV)
+	p := NewParser(hexString)
+	idWordCount := IDWordCount
+	for p.Next(idWordCount) {
+		idWordCount = IDWordCount
+		id := strings.ToUpper(string(p.ID(idWordCount)))
+		length := p.ValueLength(idWordCount)
+		hexVal := p.Value(idWordCount)
+		value, err := fromHex(hexVal)
+		if err != nil {
+			return nil, err
+		}
+		log.Printf("[ID length 2] id: %v, length: %v, hexValue: %v, value: %v", id, length, hexVal, value)
+		switch id {
+		case TagApplicationDefinitionFileName:
+			bertlv.DataApplicationDefinitionFileName = value
+		case TagApplicationLabel:
+			bertlv.DataApplicationLabel = value
+		case TagTrack2EquivalentData:
+			bertlv.DataTrack2EquivalentData = hexVal
+		case TagApplicationPAN:
+			bertlv.DataApplicationPAN = hexVal
+		default:
+			if c.customBERTLVMapID2[id] != nil {
+				if c.customBERTLVMapID2[id].IsHex {
+					bertlv.AddAdditionalData(id, hexVal)
+				} else {
+					bertlv.AddAdditionalData(id, value)
+				}
+			} else {
+				idWordCount = 4
+				id = strings.ToUpper(string(p.ID(idWordCount)))
+				length = p.ValueLength(idWordCount)
+				hexVal = p.Value(idWordCount)
+				value, err = fromHex(hexVal)
+				if err != nil {
+					return nil, err
+				}
+				log.Printf("[ID length 4] id: %v, length: %v, hexValue: %v, value: %v", id, length, hexVal, value)
+				switch id {
+				case TagCardholderName:
+					bertlv.DataCardholderName = value
+				case TagLanguagePreference:
+					bertlv.DataLanguagePreference = value
+				case TagIssuerURL:
+					bertlv.DataIssuerURL = value
+				case TagApplicationVersionNumber:
+					bertlv.DataApplicationVersionNumber = hexVal
+				case TagIssuerApplicationData:
+					bertlv.DataIssuerApplicationData = hexVal
+				case TagTokenRequestorID:
+					bertlv.DataTokenRequestorID = hexVal
+				case TagPaymentAccountReference:
+					bertlv.DataPaymentAccountReference = hexVal
+				case TagLast4DigitsOfPAN:
+					bertlv.DataLast4DigitsOfPAN = hexVal
+				case TagApplicationCryptogram:
+					bertlv.DataApplicationCryptogram = hexVal
+				case TagApplicationTransactionCounter:
+					bertlv.DataApplicationTransactionCounter = hexVal
+				case TagUnpredictableNumber:
+					bertlv.DataUnpredictableNumber = hexVal
+				default:
+					if c.customBERTLVMapID4[id] != nil {
+						if c.customBERTLVMapID4[id].IsHex {
+							bertlv.AddAdditionalData(id, hexVal)
+						} else {
+							bertlv.AddAdditionalData(id, value)
+						}
+					}
+					// skip unknown
+				}
+			}
+		}
+	}
+	return bertlv, nil
+}
+
 func format(id, value string) string {
 	length := utf8.RuneCountInString(value) / 2
 	lengthStr := strconv.Itoa(length)
@@ -136,6 +452,16 @@ func toHex(s string) string {
 	dst := make([]byte, hex.EncodedLen(len(src)))
 	hex.Encode(dst, src)
 	return string(dst)
+}
+
+func fromHex(h string) (string, error) {
+	src := []byte(h)
+	dst := make([]byte, utf8.RuneCountInString(h)/2)
+	_, err := hex.Decode(dst, src)
+	if err != nil {
+		return "", err
+	}
+	return string(dst), nil
 }
 
 func formattingTemplate(t BERTLV) string {
@@ -181,9 +507,6 @@ func formattingTemplate(t BERTLV) string {
 	}
 	if t.DataApplicationTransactionCounter != "" {
 		template += format(TagApplicationTransactionCounter, t.DataApplicationTransactionCounter)
-	}
-	if t.DataUnpredictableNumber != "" {
-		template += format(TagUnpredictableNumber, t.DataUnpredictableNumber)
 	}
 	return template
 }
